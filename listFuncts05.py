b463292bd5ec199bcd0a1577f0a652ba1a1a0cdc
Mario Vega <malbertovega@gmail.com>
2022-10-22 17:40:42 -0500

    isin, index, reverse functions added

--- listFuncts05.py
+++ listFuncts05.py
@@ -5,3 +5,24 @@ def count(myList, x):
             c = c + 1
     return c
 
+def isin(myList, x):
+    for i in myList:
+        if i == x:
+            return True
+    return False
+
+def index(myList, x):
+    c = 0
+    for i in myList:
+        if i == x:
+            return c
+        c = c + 1
+
+def reverse(myList):
+    l = len(myList)
+    new_list = [0] * l
+    index = l-1
+    for x in myList:
+        new_list[index] = x
+        index = index - 1
+    return new_list
