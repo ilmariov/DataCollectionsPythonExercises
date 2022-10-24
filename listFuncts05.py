9d1fc73a19faf9b2d1d0eb84e4dd3f165b00afde
Mario Vega <malbertovega@gmail.com>
2022-10-23 23:31:41 -0500

    all functions added, building main

--- listFuncts05.py
+++ listFuncts05.py
@@ -1,3 +1,6 @@
+from hashlib import new
+
+
 def count(myList, x):
     c = 0
     for i in myList:
@@ -26,3 +29,33 @@ def reverse(myList):
         new_list[index] = x
         index = index - 1
     return new_list
+
+def sort(myList):
+    sorted_list = []
+    while len(myList) > 0:
+        min_value = myList[0]
+        c = 0
+        index = 0
+        for x in myList:
+            if x < min_value:
+                min_value = x
+                index = c
+            c = c + 1
+        sorted_item = myList.pop(index)
+        sorted_list.append(sorted_item)
+    return sorted_list
+
+def main():
+    text = 'this is a test to see wether this function works out or not'
+    list1 = text.split()
+    list2 = [4,5,6,8,4,2,1,5,45,21,12,23,0,5,1,3,9,7,13,40]
+    print("Counting 'this':", count(list1, 'this'))
+    print("Counting 5:", count(list2, 5))
+    print("is 'or' in list:", isin(list1, 'or'))
+    print("index for 'or':", index(list1, 'or'))
+
+    print('Testing sort():')
+    print(sort(list1))
+    print(sort(list2))
+
+main()
