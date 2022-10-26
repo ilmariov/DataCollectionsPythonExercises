8b070a48173025227c183cdbde6f48d0a1cc2747
ilmariov <72055958+ilmariov@users.noreply.github.com>
2022-10-26 17:09:43 -0500

    function tested

--- removeDuplicates08.py
+++ removeDuplicates08.py
@@ -7,3 +7,9 @@ def removeDuplicates(aList):
         if not(item in new_list):
             new_list.append(item)
     return new_list
+
+def main():
+    list = removeDuplicates([2,3,5,2,6,48,65,'as','be',59,3,5,'as',2,6,'be',7,8])
+    print(list)
+
+main()
