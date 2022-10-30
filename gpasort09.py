62ff702b1962810654e2b67a85ce917602a30f17
ilmariov <72055958+ilmariov@users.noreply.github.com>
2022-10-29 23:01:50 -0500

    Update gpasort09.py

--- gpasort09.py
+++ gpasort09.py
@@ -1,2 +1,32 @@
+from gpa import Student, makeStudent
+
+def readStudents(filename):
+    infile = open(filename, 'r')
+    students = []
+    for line in infile:
+        students.append(makeStudent(line))
+    infile.close()
+    return students
+
+def writeStudents(students, filename):
+    outfile = open(filename, 'w')
+    for s in students:
+        print('{0}\t{1}\t{2}'.format(s.getName(), s.getHours(), s.getQPoints()), file=outfile)
+    outfile.close()
+
 def main():
-    pass
+    print('This program sorts student grade information by GPA')
+    filename = input('Enter the name of the data file: ')
+    data = readStudents(filename)
+    gpas = {}
+    for student in data:
+        gpas[student.gpa()] = student
+    gpas.sort()
+    std_list = []
+    for gpa in gpas:
+        std_list.append(gpas.get(gpa))
+    filename = input('Enter a name for the output file: ')
+    writeStudents(std_list, filename)
+    print('The data has been written to', filename)
+
+if __name__=='__main__': main()
