5517a96755d745c3ac4f88308f3fe314a1d02609
ilmariov <72055958+ilmariov@users.noreply.github.com>
2022-10-31 21:56:47 -0500

    sieve of eratosthenes program finished

--- sieve10.py
+++ sieve10.py
@@ -1,2 +1,22 @@
-def main():
-    pass
+def sieve():
+    n = 0
+    while (n <= 2):
+        n = int(input('Find primes up to: '))
+
+    nums = []
+    for i in range(2, n+1):
+        nums.append(i)
+    
+    primes = []
+    while(len(nums) >= 1):
+        prime = nums.pop(0)
+        primes.append(prime)
+        for num in nums:
+            if (num % prime == 0):
+                nums.remove(num)
+    
+    print('Here is the list of primes up to', n)
+    print(primes)
+
+
+sieve()
